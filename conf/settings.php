d2f1434cee13d6140e614538b148e442cb1f6e75
Neo Melonas <neo@neomelonas.com>
2009-12-07 11:38:43 -0500

    Temp fix for settings error.

--- conf/settings.php
+++ conf/settings.php
@@ -12,17 +12,7 @@
 	// If you add new ones, as of now, you need to  alter index.php to fit.
 	// Also, use the symbols provided by Yahoo! as other services (read: Google)
 	// have been known to use different ticker symbols.
-	/*/
-	$stock = array (
-		stock => ('^DJI','Dow Jones'),
-		stock => ('^IXIQ','NASDAQ'),
-		stock => ('^GSPC','S$amp;P 500')
-	);
-	
-	// When this is set to 1, shows simple memory usage.
-	// Default: 0
-	$memdebugz = 0;
-	
+	/*/	
 	$stock1 = '^DJI';
 	$sname1	= 'Dow Jones';
 
@@ -31,6 +21,10 @@
 
 	$stock3	= '^GSPC';
 	$sname3	= 'S&amp;P 500';
+	
+	// When this is set to 1, shows simple memory usage.
+	// Default: 0
+	$memdebugz = 0;
 	
 	// To counter PHP 5.3 hating SimplePie...
 	error_reporting(0);
